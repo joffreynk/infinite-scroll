1d3af583241e8a5d46ab7512980db36aa00980f5
Joffreynk <joyjoffrey@gmail.com>
2023-08-03 23:56:53 +0200

    next server actions

---
+++ app/api/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+let users: any = []
+
+export const POST = async (req: Request, {params}: {params: any}) => {
+  const {name, email} = await req.json();
+  users.push({id: users.length+1 ,name, email});
+
+  const url = new URL(req.url)
+
+  const data = Object.entries(url.searchParams)
+
+  const aaa = 
+
+  console.log('====================================');
+  console.log(url.searchParams);
+  console.log('====================================');
+
+  return new NextResponse(JSON.stringify({
+    message: "user added successfully"
+  }), {status: 201,})
+ }
+
+ export const GET = async (req: Request, res: Response) => {
+  return new NextResponse(JSON.stringify(users), {status: 201,})
+ }
+
+
+
+ 
